5d41ba911928f7de00b98b95f4a513919afa22c2
Trevor MacKay <trvmackay@gmail.com>
2022-02-16 16:18:53 -0800

    Fixed z-plane issue

--- Assets/Scripts/CaveCamera.cs
+++ Assets/Scripts/CaveCamera.cs
@@ -4,9 +4,10 @@ public class CaveCamera : MonoBehaviour{
 
     public GameObject userObject;
     public GameObject displayObject;
+
     public Vector3 userToDisplay;
 
-    public FrustumPlanes fp;
+    public float normalDistance;
 
     public void init( GameObject userObj, GameObject displayObj)
     {
@@ -23,90 +24,77 @@ public class CaveCamera : MonoBehaviour{
     public void UpdateToUserPosition()
     {
         this.gameObject.transform.position = this.userObject.transform.position;
-        //this.gameObject.transform.LookAt( this.displayObject.transform );
-        this.userToDisplay = this.displayObject.transform.position - this.gameObject.transform.position;
+        this.userToDisplay = this.displayObject.transform.position - this.userObject.transform.position;
         CalculateProjectionMatrix();
     }
 
     public void OnDrawGizmos()
     {
         GameObject thisObj = this.gameObject;
+        GameObject userObj = this.userObject;
+
         Gizmos.color = Color.white;
-        Gizmos.DrawLine( thisObj.transform.position, this.displayObject.transform.position);
+        Gizmos.DrawLine(  userObj.transform.position, userObj.transform.position + this.userToDisplay );
 
         Gizmos.color = Color.red;
         Gizmos.DrawLine( 
-            thisObj.transform.position,
-            thisObj.transform.position + new Vector3( this.userToDisplay[0], 0f, 0f )
+            userObj.transform.position,
+            userObj.transform.position + new Vector3( this.userToDisplay[0], 0f, 0f )
         );
 
         Gizmos.color = Color.blue;
         Gizmos.DrawLine( 
-            thisObj.transform.position + new Vector3( this.userToDisplay[0], 0f, 0f ),
-            thisObj.transform.position + new Vector3( this.userToDisplay[0], 0f, this.userToDisplay[2] )
+            userObj.transform.position + new Vector3( this.userToDisplay[0], 0f, 0f ),
+            userObj.transform.position + new Vector3( this.userToDisplay[0], 0f, this.userToDisplay[2] )
         );
 
         Gizmos.color = Color.green;
         Gizmos.DrawLine( 
-            thisObj.transform.position + new Vector3( this.userToDisplay[0], 0f, this.userToDisplay[2] ),
-            thisObj.transform.position + this.userToDisplay
+            userObj.transform.position + new Vector3( this.userToDisplay[0], 0f, this.userToDisplay[2] ),
+            userObj.transform.position + this.userToDisplay
         );
     }
 
     public void CalculateProjectionMatrix()
     {
+        GameObject userObj = this.userObject;
         GameObject displayObj = this.displayObject;
         CaveDisplay displayScr = displayObj.GetComponent<CaveDisplay>();
         GameObject thisObj = this.gameObject;
         Camera camera = thisObj.GetComponent<Camera>();
 
-        Vector3 eye = thisObj.transform.position;
-        Vector3 eyeToDisplay = displayScr.displayCenter - eye;
-        //camera.ResetProjectionMatrix();
+
+        this.normalDistance = Vector3.Dot( displayObj.transform.position - userObj.transform.position, displayObj.transform.forward );
+        float normalDistance = this.normalDistance;
+
+        float displayWidth = displayScr.caveDisplayTemplate.displayDimentions[0];
+        float displayHeight = displayScr.caveDisplayTemplate.displayDimentions[1];
         
-        float right = Vector3.Dot((displayScr.edgeRight - eye), displayObj.transform.right );
-        float left = Vector3.Dot((displayScr.edgeLeft - eye), displayObj.transform.right );
-        float top = Vector3.Dot((displayScr.edgeTop - eye), displayObj.transform.up );
-        float bottom = Vector3.Dot((displayScr.edgeBottom - eye), displayObj.transform.up );
-        float near = Vector3.Dot(eyeToDisplay, displayObj.transform.forward);
-        float far =  near * 50f;// displayScr.caveDisplayTemplate.drawDistance + near;
+        float near = normalDistance; //Mathf.Max(normalDistance,0.25f);
+        float far = camera.farClipPlane;
 
-        /*
-        float x = 2.0F * near / (right - left);
-        float y = 2.0F * near / (top - bottom);
-        float a = (right + left) / (right - left);
-        float b = (top + bottom) / (top - bottom);
-        float c = -(far + near) / (far - near);
-        float d = -(2.0F * far * near) / (far - near);
-        float e = -1.0F;
-        Matrix4x4 projectionM = new Matrix4x4();
-        projectionM[0, 0] = x;
-        projectionM[0, 1] = 0;
-        projectionM[0, 2] = a;
-        projectionM[0, 3] = 0;
-        projectionM[1, 0] = 0;
-        projectionM[1, 1] = y;
-        projectionM[1, 2] = b;
-        projectionM[1, 3] = 0;
-        projectionM[2, 0] = 0;
-        projectionM[2, 1] = 0;
-        projectionM[2, 2] = c;
-        projectionM[2, 3] = d;
-        projectionM[3, 0] = 0;
-        projectionM[3, 1] = 0;
-        projectionM[3, 2] = e;
-        projectionM[3, 3] = 0;
-        */
+        //float scaledDisplayWidth = Mathf.Max( 1f, Mathf.Abs( this.userToDisplay.magnitude / displayWidth));
+        //float scaledDisplayHeight = Mathf.Max( 1f, Mathf.Abs( this.userToDisplay.magnitude / displayHeight));
 
-        Matrix4x4 projectionM = Matrix4x4.Frustum(left,right,bottom,top,near,far);
-        //Matrix4x4 projectionM = Matrix4x4.Ortho(left,right,bottom,top,near,far );
-    
+        float scaledDisplayWidth = Mathf.Max( 1f, Mathf.Abs( near / displayWidth));
+        float scaledDisplayHeight = Mathf.Max( 1f, Mathf.Abs( near / displayHeight));
 
+        Vector3 scaledRight = (displayScr.edgeRight - displayObj.transform.position) * scaledDisplayWidth + displayObj.transform.position;
+        Vector3 scaledTop = (displayScr.edgeTop - displayObj.transform.position) * scaledDisplayWidth + displayObj.transform.position;
+        Vector3 scaledLeft = (displayScr.edgeLeft - displayObj.transform.position) * scaledDisplayWidth + displayObj.transform.position;
+        Vector3 scaledBottom = (displayScr.edgeBottom - displayObj.transform.position) * scaledDisplayWidth + displayObj.transform.position;
+
+        float right = Vector3.Dot((scaledRight - thisObj.transform.position), displayObj.transform.right ) ;
+        float left = Vector3.Dot((scaledLeft - thisObj.transform.position), displayObj.transform.right ) ;
+        float top = Vector3.Dot((scaledTop - thisObj.transform.position), displayObj.transform.up );
+        float bottom = Vector3.Dot((scaledBottom - thisObj.transform.position), displayObj.transform.up ) ;
 
         Vector3 dispUp = (displayScr.cornerUpperRight - displayScr.cornerLowerRight).normalized;
         Vector3 dispRight =  (displayScr.cornerUpperRight - displayScr.cornerUpperLeft).normalized;
         Vector3 dispForward = - Vector3.Cross( dispRight, dispUp );
 
+        Matrix4x4 projectionM = Matrix4x4.Frustum(left,right,bottom,top,near,far);
+    
         Matrix4x4 displayM = Matrix4x4.zero;
         displayM[0,0] = dispRight.x;
         displayM[0,1] = dispRight.y;
@@ -122,35 +110,29 @@ public class CaveCamera : MonoBehaviour{
 
         displayM[3,3] = 1f;
 
-
         Matrix4x4 translationM = Matrix4x4.Translate(
-            -eye
+            - userObj.transform.position
         );
 
         Matrix4x4 rotationM = Matrix4x4.Rotate(
-            //Quaternion.Inverse( thisObj.transform.rotation )
-                Quaternion.FromToRotation(
-                    userObject.transform.forward,
-                    displayObj.transform.forward
-
-            )
+            displayObj.transform.rotation
         );
 
-        //projectionM[2,0] *= -1;
-        //projectionM[2,1] *= -1;
-        //projectionM[2,2] *= -1;
-        //projectionM[2,3] *= -1;
+        Matrix4x4 scaleM = Matrix4x4.Scale(
+            displayObj.transform.right * scaledDisplayWidth + displayObj.transform.up * scaledDisplayHeight
+        );
 
         camera.nearClipPlane = near;
         camera.farClipPlane = far;
-
-        camera.worldToCameraMatrix = displayM * translationM;
-
+        camera.worldToCameraMatrix =  displayM * translationM;
         camera.projectionMatrix = projectionM;
+        //thisObj.transform.LookAt( displayObj.transform );
+        
     }
 
     public void Start(){
-        
+        Camera camera = this.gameObject.GetComponent<Camera>();
+        camera.Reset();
     }
     public void Update(){
         UpdateToUserPosition();
--- Assets/Scripts/CaveDisplayTemplate.cs
+++ Assets/Scripts/CaveDisplayTemplate.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 [System.Serializable]
 public class CaveDisplayTemplate
 {
     public CaveObjectReference caveObjectReference;
     public Vector2 displayDimentions = new Vector2(0.5f, 0.5f);
-    public float drawDistance = 1f;
+    public float drawDistance = 100f;
 }
