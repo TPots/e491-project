87ad4cf672158c76a8914730bc1f29cf8fa653fa
unknown <EMLWesbrook6@UIT-CDW-MEDIA02.ead.ubc.ca>
2022-03-14 14:31:42 -0700

    Added moving average to the usertraker

--- Unity_ZMQ/Assets/Scripts/UserTracker.cs
+++ Unity_ZMQ/Assets/Scripts/UserTracker.cs
@@ -2,6 +2,8 @@
 using System;
 using NetMQ;
 using NetMQ.Sockets;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PubSub {
     public class UserTracker : MonoBehaviour
@@ -37,9 +39,15 @@ namespace PubSub {
         private Quaternion user_rot;
         private SubscriberSocket subSocket;
 
+        private Queue<Orientation> orientation_queue;
+        private int orientation_max_size = 5;
+        private Orientation queue_average;
+         
+
         // Start is called before the first frame update
         void Start()
         {
+            orientation_queue = new Queue<Orientation>();
             user_pos = new Vector3(0,0,0);
             user_rot = new Quaternion(0,0,0,0);
             // Needed to handle socket exception on succesful socket connection
@@ -51,6 +59,30 @@ namespace PubSub {
             subSocket.SubscribeToAnyTopic();
         }
 
+        private void remove_value_from_average(Orientation value)
+        {
+            queue_average.pos.x -= value.pos.x / orientation_max_size;
+            queue_average.pos.y -= value.pos.y / orientation_max_size;
+            queue_average.pos.z -= value.pos.z / orientation_max_size;
+
+            queue_average.quat.x -= value.quat.x / orientation_max_size;
+            queue_average.quat.y -= value.quat.y / orientation_max_size;
+            queue_average.quat.z -= value.quat.z / orientation_max_size;
+            queue_average.quat.w -= value.quat.w / orientation_max_size;
+        }
+
+        private void add_value_to_average(Orientation value)
+        {
+            queue_average.pos.x += value.pos.x / orientation_max_size;
+            queue_average.pos.y += value.pos.y / orientation_max_size;
+            queue_average.pos.z += value.pos.z / orientation_max_size;
+
+            queue_average.quat.x += value.quat.x / orientation_max_size;
+            queue_average.quat.y += value.quat.y / orientation_max_size;
+            queue_average.quat.z += value.quat.z / orientation_max_size;
+            queue_average.quat.w += value.quat.w / orientation_max_size;
+        }
+
         // Update is called once per frame
         private void Update()
         {
@@ -58,15 +90,31 @@ namespace PubSub {
                     // Debug.Log(message);
                     try{ 
                         orientation = JsonUtility.FromJson<Orientation>(message);
-                        int factor = 100;
-                        user_pos.x = orientation.pos.x/factor;
-                        user_pos.y = orientation.pos.y/factor;
-                        user_pos.z = orientation.pos.z/factor;
-                        user_rot.x = orientation.quat.x/factor;
-                        user_rot.y = orientation.quat.y/factor;
-                        user_rot.z = orientation.quat.z/factor;
-                        user_rot.w = orientation.quat.w/factor;
+                    
+                    
+                    if (orientation_queue.Count == orientation_max_size)
+                    {
+                        Orientation removed_value = orientation_queue.Dequeue();
+                        remove_value_from_average(removed_value);
                     }
+            
+                    
+                    
+                    orientation_queue.Enqueue(orientation);
+                    add_value_to_average(orientation);
+                        int factor = 100;
+                        
+                        user_pos.x = queue_average.pos.x/factor;
+                        user_pos.y = queue_average.pos.y/factor;
+                        user_pos.z = queue_average.pos.z/factor;
+                        user_rot.x = queue_average.quat.x/factor;
+                        user_rot.y = queue_average.quat.y/factor;
+                        user_rot.z = queue_average.quat.z/factor;
+                        user_rot.w = queue_average.quat.w/factor;
+                    Debug.Log(queue_average.pos.x);
+                    Debug.Log(queue_average.pos.y);
+                    Debug.Log(queue_average.pos.z);
+                }
                     catch{
                         Debug.Log($"Failed to Parse Message: {message}");
                     }
