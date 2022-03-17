3eca4c24744cd2c2f696feba6b6d1b4c956dda8b
Elijah Hewer <phenomhewer@gmail.com>
2022-03-17 04:28:49 -0700

    Add error logging and set build architecture to x64

--- KinectSim/KinectPlayback.cs
+++ KinectSim/KinectPlayback.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿#define TRACE
+
+using System;
 using System.Numerics;
+using System.Diagnostics;
 using K4AdotNet;
 using K4AdotNet.Record;
 using K4AdotNet.Sensor;
 using K4AdotNet.BodyTracking;
-
 
 namespace KinectSim
 {
@@ -12,6 +14,11 @@ namespace KinectSim
     {
         static void Main(string[] args)
         {
+            // set up error logs
+            Trace.Listeners.Add(new TextWriterTraceListener("traceErrors.log"));
+            Trace.AutoFlush = true;
+
+
             // initialize server at port 12345
             KinectZMQ.ZMQServer server = new KinectZMQ.ZMQServer(12345);
 
@@ -19,51 +26,59 @@ namespace KinectSim
             Console.WriteLine("Enter the name of the recording: ");
             string filename = Console.ReadLine();
 
-            // open playback
-            using (Playback playback = new Playback(filename))
+            try
             {
-                var length = playback.RecordLength;
-
-                // setup tracker based on recording
-                TrackerConfiguration trackerConfiguration = new TrackerConfiguration();
-                playback.GetCalibration(out Calibration calibration);
-
-                using (Tracker tracker = new Tracker(calibration, trackerConfiguration))
+                // open playback
+                using (Playback playback = new Playback(filename))
                 {
-                    // while frame still available
-                    while (playback.TryGetNextCapture(out Capture capture))
-                    {
-                        // add sensor capture to tracker queue with infinite timeout
-                        using (capture)
-                        {
-                            tracker.EnqueueCapture(capture);
-                        }
+                    var length = playback.RecordLength;
 
-                        // get latest tracker frame with infinite timeout
-                        using (BodyFrame frame = tracker.PopResult())
+                    // setup tracker based on recording
+                    TrackerConfiguration trackerConfiguration = new TrackerConfiguration();
+                    playback.GetCalibration(out Calibration calibration);
+
+                    using (Tracker tracker = new Tracker(calibration, trackerConfiguration))
+                    {
+                        // while frame still available
+                        while (playback.TryGetNextCapture(out Capture capture))
                         {
-                            if (frame.BodyCount != 1)
+                            // add sensor capture to tracker queue with infinite timeout
+                            using (capture)
                             {
-                                continue;
+                                tracker.EnqueueCapture(capture);
                             }
 
-                            frame.GetBodySkeleton(0, out Skeleton skeleton);
+                            // get latest tracker frame with infinite timeout
+                            using (BodyFrame frame = tracker.PopResult())
+                            {
+                                if (frame.BodyCount != 1)
+                                {
+                                    continue;
+                                }
 
-                            Joint eyeRight = skeleton.EyeRight;
-                            Joint eyeLeft = skeleton.EyeLeft;
-                            // convert K4AdotNet.Quaternion to System.Numerics.Quaternion
-                            System.Numerics.Quaternion quaternion = new System.Numerics.Quaternion(
-                                eyeRight.Orientation.X,
-                                eyeRight.Orientation.Y,
-                                eyeRight.Orientation.Z,
-                                eyeRight.Orientation.W
-                            ); 
-                            server.PublishData(Vector3(eyeRight.PositionMm), quaternion); 
+                                frame.GetBodySkeleton(0, out Skeleton skeleton);
+
+                                Joint eyeRight = skeleton.EyeRight;
+                                Joint eyeLeft = skeleton.EyeLeft;
+                                // convert K4AdotNet.Quaternion to System.Numerics.Quaternion
+                                System.Numerics.Quaternion quaternion = new System.Numerics.Quaternion(
+                                    eyeRight.Orientation.X,
+                                    eyeRight.Orientation.Y,
+                                    eyeRight.Orientation.Z,
+                                    eyeRight.Orientation.W
+                                );
+                                server.PublishData(Vector3(eyeRight.PositionMm), quaternion);
+                            }
                         }
-                    }
 
-                    Console.WriteLine("End of file.");
+                        Console.WriteLine("End of file.");
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("-----START OF ERROR LOG-----");
+                Trace.WriteLine(ex);
             }
         }
 
