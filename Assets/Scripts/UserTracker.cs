89570afd0d7624ba4471d30f04e5021d539872f8
Aviral-Salhotra <aviralsalhotra2@gmail.com>
2022-04-07 01:03:12 -0700

    Removed stale demo files

--- Assets/Scripts/UserTracker.cs
+++ Assets/Scripts/UserTracker.cs
@@ -64,10 +64,7 @@ namespace PubSub {
             Debug.Log($"Connecting Sub Socket to address: tcp://{host}:{port}");       
             subSocket.Connect($"tcp://{host}:{port}");
             subSocket.SubscribeToAnyTopic();
-
             zmqThread = new Thread(getZMQMessage);
-            zmqThread.Start();
-
         }
 
         private void remove_value_from_average(Orientation value)
@@ -136,45 +133,9 @@ namespace PubSub {
         // Update is called once per frame
         private void Update()
         {
-            // if (setupScr.caveSetupTemplate.trackUser){
-            //     int factor = 1000;
-            //     if (subSocket.TryReceiveFrameString(out var message)){
-            //             //Debug.Log("Recieved Data");
-            //             //Debug.Log(message);
-            //             try{ 
-            //                 orientation = JsonUtility.FromJson<Orientation>(message);
-                        
-                        
-            //             if (orientation_queue.Count == orientation_max_size)
-            //             {
-            //                 Orientation removed_value = orientation_queue.Dequeue();
-            //                 remove_value_from_average(removed_value);
-            //             }
-                
-                        
-                        
-            //             orientation_queue.Enqueue(orientation);
-            //             add_value_to_average(orientation);
-            //                 user_pos.x = queue_average.pos.x/-factor;
-            //                 user_pos.y = queue_average.pos.y/-factor;
-            //                 user_pos.z = queue_average.pos.z/-factor;
-            //                 user_rot.x = queue_average.quat.x/factor;
-            //                 user_rot.y = queue_average.quat.y/factor;
-            //                 user_rot.z = queue_average.quat.z/factor;
-            //                 user_rot.w = queue_average.quat.w/factor;
-            //             // Debug.Log(queue_average.pos.x);
-            //             // Debug.Log(queue_average.pos.y);
-            //             // Debug.Log(queue_average.pos.z);
-            //         }
-            //             catch{
-            //                 Debug.Log($"Failed to Parse Message: {message}");
-            //             }
-            //     setupScr.UpdateFromZMQ(user_pos, Vector3.zero);
-            // }
-
-            //gameObject.transform.SetPositionAndRotation( user_pos,user_rot);
-
-        //}
+            if (setupScr.caveSetupTemplate.trackUser && !zmqThread.IsAlive){
+                zmqThread.Start();
+            }
         }
 
     void OnApplicationQuit()
